585d4b7ca49f609660ac5f6a6d1b3d1577a604b7
shutterwolf <pistelli.info@gmail.com>
2026-02-23 18:00:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -543,7 +543,7 @@ class MyRoom extends Room {
                 if (!enemy.stuckSince) enemy.stuckSince = now;
             }
         }
-};
+}
 
         this.onMessage("requestSpawnEnemies", (client, data) => {
                 console.log("🔔 requestSpawnEnemies ricevuto");
@@ -941,6 +941,7 @@ const PORT = process.env.PORT || 10000;
 httpServer.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
 
 
 
