ceb5f999fe5aa5944ee109261aba1a17df6a1a59
shutterwolf <pistelli.info@gmail.com>
2026-06-14 17:08:01 +0200

    Create npcNames.js

---
+++ npcNames.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    male: [
+        "Dorian",
+        "Borin",
+        "Cedric",
+        "Thoran",
+        "Kael",
+        "Rurik",
+        "Garrick",
+        "Alric",
+        "Varek",
+        "Arden"
+    ],
+
+    female: [
+        "Elysa",
+        "Lysa",
+        "Selene",
+        "Mira",
+        "Elara",
+        "Nadia",
+        "Ariana",
+        "Lyra",
+        "Seris",
+        "Talia"
+    ]
+
+};
