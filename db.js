e18f5d24ac12d4a388eb109d0e7fc998147160e8
shutterwolf <pistelli.info@gmail.com>
2026-06-25 18:42:24 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 console.log(serviceAccount.project_id);
 console.log(serviceAccount.client_email);
+console.log(serviceAccount.private_key.startsWith("-----BEGIN PRIVATE KEY-----"));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
