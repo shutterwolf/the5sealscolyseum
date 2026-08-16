6ab9b0d2022f7837b655866bbc50b4bafc27e57f
shutterwolf <pistelli.info@gmail.com>
2026-08-16 18:03:25 +0200

    Create specials.js

---
+++ specials.js
@@ -0,0 +1,50 @@
+// specials.js
+module.exports = {
+  // Enigmi
+  LEVER:          10,
+  BUTTON:         11,
+  PRESSURE_PLATE: 12,
+  KEYHOLE:        13,
+  RUNE_STONE:     14,
+  ALTAR:          15,
+  ROTATING_GEAR:  16,
+  MIRROR:         17,
+  PULL_CHAIN:     18,
+  TORCH_BRACKET:  19,
+
+  // Trappole
+  ARROW_TRAP:     20,
+  SPIKE_TRAP:     21,
+  CEILING_TRAP:   22,
+  GAS_TRAP:       23,
+  PIT_TRAP:       24,
+  BLADE_TRAP:     25,
+  FIRE_TRAP:      26,
+  ICE_TRAP:       27,
+  CRUSHING_WALL:  28,
+  MAGIC_TRAP:     29,
+
+  // Porte speciali
+  DOOR_KEY:       30,
+  DOOR_PUZZLE:    31,
+  DOOR_SECRET:    32,
+  DOOR_ONEWAY:    33,
+  DOOR_TIMED:     34,
+  DOOR_HEAVY:     35,
+
+  // Decorazioni interattive
+  VASE:           40,
+  BOOKCASE:       41,
+  STATUE:         42,
+  BARREL:         43,
+  COBWEB:         44,
+  HANGING_CHAIN:  45,
+  FOUNTAIN:       46,
+  LECTERN:        47,   // Leggio: apre testo
+  PEDESTAL:       48,   // Piedistallo: appoggia oggetto
+
+  // Ricompense
+  CHEST:          50,
+  SHRINE:         51,
+  LOOT_CORPSE:    52
+};
